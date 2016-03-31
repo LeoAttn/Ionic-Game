dc1afdd3d8a15117f6832e615f311e81917c7b12
bluedragonfly <noki.dev@gmail.com>
2016-03-31 14:57:58 +0200

    Commit SALE !2

--- app/components/hero-service.js
+++ app/components/hero-service.js
@@ -7,14 +7,11 @@ import {StorageService} from './storage-service'
 @Injectable()
 export class HeroService{
     constructor(storage:StorageService){
-        storage.remove("playerName");
-        storage.remove("playerMoney");
-        storage.remove("playerDamage");
         this.name = storage.initOrGet("playerName", "");
         this.money = storage.initOrGet("playerMoney", 0);
         this.attackDamage = storage.initOrGet("playerDamage", 1);
         this.level = storage.initOrGet("playerLevel", 1);
-        this.spells = JSON.parse(storage.initOrGet("equippedSpells", []));//@Need to test this
+        this.spells = storage.initOrGet("equippedSpells", []);//@Need to test this
     }
 }
 
