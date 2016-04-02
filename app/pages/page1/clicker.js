0f4265ed8bb0795695fc159ea4b1d42b419e3d6e
Léo <leo.massonnat@hotmail.fr>
2016-04-02 15:28:45 +0200

    level up

--- app/pages/page1/clicker.js
+++ app/pages/page1/clicker.js
@@ -1,7 +1,8 @@
 import {Component} from 'angular2/core'
-import {Storage, LocalStorage} from 'ionic-angular'
+import {Storage, LocalStorage, IONIC_DIRECTIVES} from 'ionic-angular'
 import {Subject, Observable} from 'rxjs'
 import {StorageService} from '../../components/storage-service'
+import {HeroService} from '../../components/hero-service'
 
 const MONSTER_LIFE = 'monster_life_key';
 const LEVEL = 'level_key';
@@ -10,12 +11,13 @@ const LEVEL = 'level_key';
     selector: 'clicker',
     template: `
         <h2>Level : {{levelStream | async}}</h2>
-        <h2>Value : {{healthStream | async}}</h2>
-        <button (click)="clickStream.next()" id="clicBtn">Increment</button>
-    `
+        <h2>Health : {{healthStream | async}}</h2>
+        <button round large (click)="clickStream.next()" id="clicBtn">OUTCH</button>
+    `,
+    directives: [IONIC_DIRECTIVES]
 })
 export class Clicker {
-    constructor(localStorage:StorageService) {
+    constructor(localStorage:StorageService, hero:HeroService) {
         localStorage.set(MONSTER_LIFE, 15);
         const savedLife = localStorage.get(MONSTER_LIFE)
             .map(toIntOrZero);
@@ -27,22 +29,22 @@ export class Clicker {
 
         const damage = this.clickStream
             .map(click => 1)
-            .scan((a, b) => a + b)
+            .scan((a, b) => a + b);
 
-        const lostHealthStream = Observable
-            .combineLatest(damage, savedLife, (damage, life) => damage % life)
-            .startWith(0)
-
-        this.healthStream = Observable
-            .combineLatest(lostHealthStream, savedLife, (lostHealth, life) => life - lostHealth)
+        this.healthStream = hero.attack(savedLife, damage);
 
         this.healthStream.subscribe(health => {
-            localStorage.set(MONSTER_LIFE, health)
+            localStorage.set(MONSTER_LIFE, health);
             if (health == 15) {
+                console.log('plop');
                 this.levelStream = this.levelStream.map(level => level + 1)
+                    .do(level => console.log('level ' + level));
             }
         });
-        this.levelStream.subscribe(count => localStorage.set(LEVEL, count));
+        this.levelStream.subscribe(count => {
+            console.log('UPDATE');
+            localStorage.set(LEVEL, count);
+        });
     }
 }
 
